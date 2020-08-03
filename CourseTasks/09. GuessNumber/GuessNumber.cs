d5e36e33935769e91f66686950156db2aa38f71f
VladislavAgeev <Vlad32927@yandex.ru>
2020-08-03 16:28:21 +0700

    Add Courses

---
+++ CourseTasks/09. GuessNumber/GuessNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class GuessNumber
+    {
+        static void Main(string[] args)
+        {
+            Random x = new Random();
+            int hiddenNumber = x.Next(1, 101);
+
+            Console.Write("Отгадайте число от 1 до 100: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            int attemptsCount = 1;
+
+            for (; ; )
+            {
+                if (userNumber > hiddenNumber)
+                {
+                    Console.Write("Загаданное число меньше введенного! Повторите ввод: ");
+                    userNumber = Convert.ToInt32(Console.ReadLine());
+                    attemptsCount++;
+                }
+                else if (userNumber < hiddenNumber)
+                {
+                    Console.Write("Загаданное число больше введенного! Повторите ввод: ");
+                    userNumber = Convert.ToInt32(Console.ReadLine());
+                    attemptsCount++;
+                }
+                else
+                {
+                    Console.WriteLine("Вы отгадали число!");
+                    Console.WriteLine("Попыток использовано = " + attemptsCount);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CourseTasks/10. Contribution/Contribution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Contribution
+{
+    class Contribution
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сумму вклада: ");
+            double startDeposit = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите ставку % годовых: ");
+            double rate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите срок вклада: ");
+            int period = Convert.ToInt32(Console.ReadLine());
+
+            double epsilon = 1.0e-5;
+
+            if (rate < 0)
+            {
+                Console.WriteLine("Ставка % годовых не может быть отрицательной");
+            }
+            else if (rate <= epsilon)
+            {
+                Console.WriteLine("По истечении срока вклада сумма составит: {0}", startDeposit);
+            }
+            else
+            {
+                const int months = 12;
+                const int oneHundredthOfRate = 100;
+
+                double tempRate = rate / oneHundredthOfRate / months;
+                double tempDeposit = startDeposit;
+
+                for (int i = 1; i <= period; i++)
+                {
+                    tempDeposit += tempDeposit * tempRate;
+                }
+
+                double profit = tempDeposit - startDeposit;
+
+                Console.WriteLine("По истечении срока вклада сумма составит: {0}", Math.Round(tempDeposit, 2));
+                Console.WriteLine("Прибыль составила: {0}", Math.Round(profit, 2));
+            }
+        }
+    }
+}
