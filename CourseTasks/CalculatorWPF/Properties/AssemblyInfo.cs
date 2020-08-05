6cf846594843da37765b734a5db77c4c97f484ae
VladislavAgeev <Vlad32927@yandex.ru>
2020-08-05 13:13:00 +0700

    fixes in CalculatorWPF

--- CourseTasks/CalculatorWPF/Properties/AssemblyInfo.cs
+++ CourseTasks/CalculatorWPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyTitle("CalculatorWPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Calculator")]
+[assembly: AssemblyProduct("CalculatorWPF")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
